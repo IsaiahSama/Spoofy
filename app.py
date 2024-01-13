e170ee6e74655d35599fc28e8bc7463df50f1033
Isaiah-Sama <zeldaplayergl@gmail.com>
2024-01-13 16:47:59 -0400

    Added beep bop boop (Trust the process)

--- app.py
+++ app.py
@@ -3,14 +3,24 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/something")
+@app.route("/")
+def index():
+    return "<a href='/beep/'>Beep</a><a href='/bop/'>Bop</a><a href='/boop/'>Boop</a>"
+
+
+@app.route("/beep/")
 def beep():
-    return "<h1>Hello there</h1>"
+    return "<h1>Beep</h1>"
 
 
-@app.route("/hi")
+@app.route("/bop/")
+def bop():
+    return "<h1>Bop</h1>"
+
+
+@app.route("/boop/")
 def boop():
-    return "<h1>Hi there</h1>"
+    return "<h1>Boop</h1>"
 
 
 if __name__ == "__main__":
