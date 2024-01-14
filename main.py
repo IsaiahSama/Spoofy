25a51d3b69f5bc89dd4646a94d123c11129eda05
Isaiah-Sama <zeldaplayergl@gmail.com>
2024-01-13 20:24:44 -0400

    Updated automation

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ load_dotenv()
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
+from sele import download_songs_by_ids
+
 scope = "user-library-read"
 
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
@@ -11,8 +13,13 @@ sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
 i = 0
 results = sp.current_user_saved_tracks(50)
 while results["items"]:
+    ids = []
     for idx, item in enumerate(results["items"], start=50 * i):
         track = item["track"]
-        print(idx, track["artists"][0]["name"], " – ", track["name"])
+        track_id = track["id"]
+        ids.append(track_id)
+
+    download_songs_by_ids(ids)
+
     i += 1
     results = sp.current_user_saved_tracks(50, 50 * i)
--- sele.py
+++ sele.py
@@ -8,30 +8,25 @@ URL = "https://spotifymate.com/"
 
 base = "https://open.spotify.com/track/"
 
-ids = """12TP8c0mlvpZpKo1YhVcKc
-5iQDeHszlApK5qgRaqEBTv
-28ADc91k22VcUqT4ao4Z8s""".split(
-    "\n"
-)
 
-for song_id in ids:
-    driver = webdriver.Edge()
+def download_songs_by_ids(ids: list):
+    for song_id in ids:
+        driver = webdriver.Edge()
 
-    driver.get(URL)
+        driver.get(URL)
 
-    elem = driver.find_element(By.ID, "url")
-    elem.clear()
-    elem.send_keys(base + song_id)
-    elem.send_keys(Keys.RETURN)
-    found = False
-    while not found:
-        try:
-            img = locateCenterOnScreen("./image.png", confidence=0.7)
-            found = True
-        except ImageNotFoundException:
-            pass
+        elem = driver.find_element(By.ID, "url")
+        elem.clear()
+        elem.send_keys(base + song_id)
+        elem.send_keys(Keys.RETURN)
+        found = False
+        while not found:
+            try:
+                img = locateCenterOnScreen("./image.png", confidence=0.7)
+                found = True
+            except ImageNotFoundException:
+                pass
 
-    click(img.x, img.y)
-    sleep(6)
-
-input()
+        click(img.x, img.y)
+        sleep(10)
+        driver.close()
